537ced9ae10678adaff78b6c62e22e38e2746c49
Brian Salemi <briandsalemi@gmail.com>
2020-01-11 01:17:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  messages = []
+  attendees.each do |name|
+    messages.push(badge_maker(name))
+  end
+  return messages
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index do |name, index|
+    room_assignments.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
+  end
+  return room_assignments
+end
+
+def printer
